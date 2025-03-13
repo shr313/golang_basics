f6db3a2cbac82821231061d71e6823df1114a8ed
Sahar <s.sid313@outlook.com>
2025-03-13 13:41:54 +0000

    Control Flow, Itteration, Arrays, Slice & Maps

--- main.go
+++ main.go
@@ -1,78 +1,128 @@
 package main
 
+//  import format package
 import (
 	"fmt"
 )
 
 // Main Function
-func main() {
-	// Variable declaration/use
-	//no unused variable allowed
+func main(){
 
-	var name string = " Muhammad "// type inferred and shorthand  operator(:=)
-	age := 414
-	fmt.Printf(" This is my first name is %s\n and I am %d\n years old,", name, age)
+	// control Structure
+	age := 30
+	if age >= 18 {
+		fmt.Println(" You are an adult,your ticket will be £30  ")
+	}else if age>= 13{
+		fmt.Println("You are an teenager,your ticket will be £18")
+	}else {
+		fmt.Println("You are a child,your ticket will be £12")
+	}
+	// Switch Statement
+day := "Tuesday"
+switch day{
+	case "Monday":
+	fmt.Printf(" its %s Start of the week.\n ",day)
+	case "Tuesday","Wednesday","Thursday":
+	fmt.Printf("It's %s midweek.\n ",day)
+	case "Friday":
+		fmt.Printf("It's a %s funday\n ",day)
+		default:
+		fmt.Printf("it's the %s weekend\n",day)
+}
+// For Loop
+for i := 0; i <5; i++ {
+	fmt.Printf("This is... %d\n", i)
+}
+// While Loop
+ counter := 0
+ for counter <5{
+	fmt.Printf("Count....%d\n", counter)
+	counter++ // use ++(condition) with variable otherwise it'll never stop.
+ }
 
-	var city string // variable declaration without initialization
-	city = "Great London"
-	fmt.Printf(" and I live in %s.\n", city)
+ //infinite Loop
+iteration := 0
+ for{
+	if iteration >3 {
+		fmt.Printf("Iteration is %d\n",iteration)
+		break//some condition is met
+	}
+	iteration++
+ }
 
-	var country, continent string = "England", "Europe"
-	fmt.Printf("My country is %s\n and continent is %s\n", country, continent)
+ // Array and Slices#
+ numbers := [5]int{10,20,30,40,50}
+ fmt.Printf("This is my Array... %d\n", numbers)
+ //find out the last value
+ fmt.Printf("This is the last value in my array is %d\n",numbers[len(numbers)-1])
+ // find out the length of array
+ fmt.Printf("This is the length of my array %d\n", len(numbers))
+ //find the first value in array
+ fmt.Printf("This is the first value in  my array %d\n", numbers[0])
 
-var (
-	isEmployed bool = true
-	salary int = 52000
-	position string = "Data scientist")
-	fmt.Printf("isEmployed : %t\n this is my Yearly %d\n salary and my position is %s\n", isEmployed, salary, position)
+ // if we dont know the length of array we can use[...] in the place of [length]like
+//  numberAtInit := [...]int{10,20,30,40,850}
 
-	// zero Values, compiler will assign the values
-	var defaultInt int
-	var defaultFloat float64
-	var defaultString string
-	var defaultBool bool
-
-	fmt.Printf("default int: %d\n default string: %s\n default float: %f\n default bool: %t\n", defaultInt,defaultString,defaultFloat,defaultBool)
-
-// Const Values
-	const pi = 3.14 //const values can be left unuse
-
-// declaring const variable with same type
-	const (
-		Monday = 1
-		Tuesday = 2
-		Wednesday = 3
-	)
-	fmt.Printf("  Monday: %d\n  Tuesday: %d\n  Wednesday: %d\n", Monday, Tuesday, Wednesday)
-
-	const typedAge int = 25
-	const untypedAge = 25
-	fmt.Println(typedAge == untypedAge)
-
-// Enum like Structure
-	const(
-		Jan = iota +1
-		Feb
-		March
-		April
-		May
-	)
-	fmt.Printf(" Jan: %d\n Feb: %d\n March: %d\n April: %d\n May: %d\n", Jan, Feb, March, April,May)
-
-//add Function
-result := add(625, 5483)
-fmt.Printf("This is the result %d ", result)
-
-//Multiple Return Types
-sum, product := calculateSumAndProduct(2 ,5)
-fmt.Printf("The sum is %d\n and the product is %d\n", sum, product)
+// Matrix Array
+matrix := [2][3]int{
+	{1,2,3},
+	{5,6,7},
+// the [2]representing the the two array vales in the array and [3] is representing the values in array
 }
 
-//add Function
+fmt.Printf("The MUlti-Dimensional Array is %v\n",matrix)
+//slices are dimensional array
+// allnumbers := numberss[:]
+// firstThree := numbers[0:3]
+
+//String Array
+fruits :=[]string{"apple","banana","Pineapple"}
+fmt.Printf("This is fruit array %v\n", fruits)
+fruits = append(fruits, "Mango")// if you need to add something else in array
+fmt.Printf("This is fruit array %v\n", fruits)
+
+moreFruits := []string{"Kiwi", "Strawberry"}
+fruits = append(fruits, moreFruits...)
+fmt.Printf("This is my new fruits array %v\n", fruits)
+
+// make statements / pre allocated memory of slices
+
+scores := make([]int,3) //slice with the length 3 and capacity 3
+scoresWithExplicitCapicity := make([]int, 3, 5)//slice with the length 3 and capacity 5
+fmt.Println("scoreWithExplicitCapacity",len(scoresWithExplicitCapicity),cap(scoresWithExplicitCapicity))
+fmt.Println("scoreWithExplicitCapacity",len(scores),cap(scores))
+// length is the current amount of elements present in the array or slice.
+//capacity is the total number of element that can have/ can be added to the array.
+
+for index, value := range numbers{
+	fmt.Printf("index %d and value %d\n",index, value)
+}
+// we use(-,) to void any variable
+// or _, value := range numbers{
+// 	fmt.Printf("index %d and value %d\n", value)
+
+
+capitalCities := map[string]string{
+	"USA" : "Washington D.C.",
+	"Italy" : "Rome",
+	"England" : "London",
+}
+fmt.Println(capitalCities["Italy"])
+//if we want to anything exits
+capital, exits := capitalCities["Germany"]
+if exits { // exits keyword only use in the map
+	fmt.Printf("This is the capital of %v\n",capital)
+} else{
+		fmt.Println("This does not exits.")
+	}
+}
+
+// add function
 func add(a int, b int) int {
-	return(a+b)
+	return a+b
 }
-// Multiple Return Types
+
+// Multiple Use of Variable in function
 func calculateSumAndProduct(a,b int)(int, int){
-	return a+b,a*b
+	return a+b, a*b
 }
